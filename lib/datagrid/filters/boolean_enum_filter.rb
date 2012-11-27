0f83d5670062daf99e82915a8baef9b3d4d5cc01
Michael Pearson <mipearson@gmail.com>
2012-11-27 12:27:27 +1100

    BooleanEnumFilter to use true/false.
    AR interprets both 'YES' & 'NO' as false.

--- lib/datagrid/filters/boolean_enum_filter.rb
+++ lib/datagrid/filters/boolean_enum_filter.rb
@@ -4,8 +4,10 @@ class Datagrid::Filters::BooleanEnumFilter < Datagrid::Filters::EnumFilter
   NO = "NO"
 
   def initialize(report, attribute, options = {}, &block)
+    options[:allow_blank] = true
+    options[:allow_nil] = false
     options[:select] = [YES, NO].map do |key, value|
-      [I18n.t("datagrid.filters.eboolean.#{key.downcase}", :default => key.humanize), key]
+      [I18n.t("datagrid.filters.eboolean.#{key.downcase}", :default => key.humanize), key == 'YES' ? true : false]
     end
     super(report, attribute, options, &block)
   end
@@ -16,6 +18,7 @@ class Datagrid::Filters::BooleanEnumFilter < Datagrid::Filters::EnumFilter
 
   def to_boolean(value)
     #TODO decide what to do with conversion
+    value.blank? ? nil : value
   end
 
 end
